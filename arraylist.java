9f5dea68c80841af9e231925fd2c18f3d0b326ba
Jithu666 <98675683+Jithu666@users.noreply.github.com>
2024-06-08 12:17:15 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+
+public class arraylist {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(6);
+
+        System.out.println(list);
+
+    }
+}
